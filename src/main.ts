e4e0cdc2445711e1bc77ddca6e173af81fc319b5
CalmBit <EB5473@gmail.com>
2019-12-27 10:54:56 -0500

    Added actual secret handling

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function run() {
       core.setFailed("Bad `status` type '" + s + "'.")
       return
     }
-    let url = process.env.WEBHOOK_URL || "null"
+    let url = process.env.MSTEAMS_WEBHOOK_URL || core.getInput("webhook_url")
     request.post(url, { json: true, body: generateCard(), }, function (err, resp, body) {
       if(err) {
         core.setFailed(err)
